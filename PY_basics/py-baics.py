cc3b042c553789650c6d01548422c6edde3e22a5
Riri-W <Naturawsone@gmail.com>
2025-04-14 09:39:48 +0100

    Update py-baics.py

--- PY_basics/py-baics.py
+++ PY_basics/py-baics.py
@@ -1,18 +1,34 @@
->>> def linear_search (arr,target):
-...     for i in range (len (arr)):
-...         if arr [i] == target:
-...             return i    # found, return index
-...
-...             return -1   # Not found
-...
->>> arr= [10,20,30,40,50]
->>>
->>> arr= [10,20,30,40,50]
->>>
->>> target=40
->>> print (linear_search (arr,target))
-3
->>>
+def merge_sort(arr):
+    if len(arr) > 1:
+        mid = len(arr) // 2
+        left_half = arr[:mid]
+        right_half = arr[mid:]
 
+        merge_sort(left_half)
+        merge_sort(right_half)
 
+        i = j = k = 0
 
+        while i < len(left_half) and j < len(right_half):
+            if left_half[i] < right_half[j]:
+                arr[k] = left_half[i]
+                i += 1
+            else:
+                arr[k] = right_half[j]
+                j += 1
+            k += 1
+
+        while i < len(left_half):
+            arr[k] = left_half[i]
+            i += 1
+            k += 1
+
+        while j < len(right_half):
+            arr[k] = right_half[j]
+            j += 1
+            k += 1
+
+# Example usage
+arr = [8,7,6,5,4,3,2,1]
+merge_sort(arr)
+print("Sorted array is:", arr)
